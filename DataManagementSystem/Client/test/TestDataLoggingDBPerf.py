e8e3c11ee30ffd8d9e718fa6a28d469bcfab5ef7
Corentin Berger <corent.berger@gmail.com>
2015-06-18 13:25:03 +0200

    save from rollback

--- DataManagementSystem/Client/test/TestDataLoggingDBPerf.py
+++ DataManagementSystem/Client/test/TestDataLoggingDBPerf.py
@@ -5,7 +5,7 @@ Created on Jun 11, 2015
 '''
 
 import time
-
+import random
 from DIRAC.DataManagementSystem.Client.DataLoggingClient import DataLoggingClient
 from DIRAC.DataManagementSystem.Client.DLSequence import DLSequence
 from DIRAC.DataManagementSystem.Client.DLAction import DLAction
@@ -15,22 +15,29 @@ from DIRAC.DataManagementSystem.Client.DLStorageElement import DLStorageElement
 from DIRAC.DataManagementSystem.Client.DLMethodName import DLMethodName
 
 
-dictLong = {'files': 'first', 'targetSE': 'longTargetSE',
+dictLong = {'files': 'second', 'targetSE': 'longTargetSE',
  'blob': 'physicalFile = blablablablablabla ,fileSize = 6536589', 'srcSE': 'longSrcSE'}
+
+randomMax = 100
 
 def makeSequenceA():
   sequence = DLSequence()
   sequence.setCaller( 'longCallerName' )
   calls = []
-  for x in range( 5000 ):
-    calls.append( sequence.appendMethodCall( {'name': DLMethodName( 'longMethodName2' + str( x ) )} ) )
 
-  for x in range( 5000 ):
-    sequence.popMethodCall()
+  calls.append( sequence.appendMethodCall( {'name': DLMethodName( 'longMethodName2' )} ) )
+  calls.append( sequence.appendMethodCall( {'name': DLMethodName( 'longMethodName2' )} ) )
+  sequence.popMethodCall()
+  calls.append( sequence.appendMethodCall( {'name': DLMethodName( 'longMethodName3' )} ) )
+  sequence.popMethodCall()
+  sequence.popMethodCall()
 
   for call in calls :
-    call.addAction( DLAction( DLFile( dictLong['files'] ), DLStatus( 'Successful' ) ,
-              DLStorageElement( dictLong['srcSE'] ), DLStorageElement( dictLong['targetSE'] ), dictLong['blob'], 'errorMessage' ) )
+    for x in range( 1 ):
+      call.addAction( DLAction( DLFile( dictLong['files'] ), DLStatus( 'Successful' ) ,
+            DLStorageElement( dictLong['srcSE'] + str( random.randint( 0, randomMax ) ) ),
+            DLStorageElement( dictLong['targetSE'] + str( random.randint( 0, randomMax ) ) ),
+            dictLong['blob'], 'errorMessage' ) )
   return sequence
 
 
@@ -38,14 +45,14 @@ def makeSequenceB():
   sequence = DLSequence()
   sequence.setCaller( 'longCallerName' )
   calls = []
-  for x in range( 5 ):
+  for x in range( 10 ):
     calls.append( sequence.appendMethodCall( {'name': DLMethodName( 'longMethodName2' + str( x ) )} ) )
 
-  for x in range( 5 ):
+  for x in range( 10 ):
     sequence.popMethodCall()
 
   for call in calls :
-    for x in range( 20000 ):
+    for x in range( 5 ):
       call.addAction( DLAction( DLFile( dictLong['files'] ), DLStatus( 'Successful' ) ,
               DLStorageElement( dictLong['srcSE'] + str( x % 20 ) ), DLStorageElement( dictLong['targetSE'] + str( x % 20 ) ),
               dictLong['blob'], 'errorMessage' ) )
@@ -59,14 +66,14 @@ print time.strftime( '%H :%M : %S', time.localtime( time.time() ) )
 
 begin = time.time()
 for x in range( 1 ) :
-  seq = makeSequenceB()
-  print 'begin insertion'
+  seq = makeSequenceA()
   begin = time.time()
   res = client.insertSequence( seq )
   if not res['OK']:
     print 'res %s' % res['Message']
   t = time.time() - begin
-  print 'end of insertion, time : %s' % time.strftime( '%M : %S', time.localtime( t ) )
+  if ( x % 100 ) == 0 :
+    print 'end of insertion number %s , time : %s' % ( x, t )
 
 print time.strftime( '%H : %M : %S', time.localtime( time.time() ) )
 
