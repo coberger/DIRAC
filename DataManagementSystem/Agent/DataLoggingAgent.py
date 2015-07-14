7ca7f3c7e0a7368f83aea950e7820b78525fc69d
Corentin Berger <corent.berger@gmail.com>
2015-07-14 16:48:43 +0200

    datalogging agent

---
+++ DataManagementSystem/Agent/DataLoggingAgent.py
@@ -0,0 +1,19 @@
+'''
+Created on Jun 19, 2015
+
+@author: Corentin Berger
+'''
+
+
+from DIRAC.Core.Base.AgentModule import AgentModule
+from DIRAC.DataManagementSystem.Client.test.mockDirac import ClientB
+
+# # agent's name
+AGENT_NAME = 'DataManagement/DataLoggingAgent'
+
+########################################################################
+class DataLoggingAgent( AgentModule ):
+
+  def test_agentName(self):
+    client = ClientB()
+    client.doSomething()
